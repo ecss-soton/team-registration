ed6f32a9851f00b78b2aa4d2727f06a27a1f1cd1
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2022-08-27 16:25:18 +0100

    Add redirect if an error occurs

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,64 +5,71 @@ import DiscordProvider from "next-auth/providers/discord";
 import {PrismaAdapter} from "@next-auth/prisma-adapter"
 import prisma from "../../../prisma/client";
 import axios from "axios";
+import {NextApiRequest, NextApiResponse} from "next";
 
-export default NextAuth({
-    adapter: PrismaAdapter(prisma),
-    pages: {
-        signIn: '/',
-        signOut: '/',
-    },
-    providers: [
-        AzureADProvider({
-            clientId: String(process.env.AZURE_AD_CLIENT_ID),
-            clientSecret: String(process.env.AZURE_AD_CLIENT_SECRET),
-            tenantId: process.env.AZURE_AD_TENANT_ID,
-            authorization: {
-                params: {
-                    scope: "openid profile User.Read email",
-                },
-            },
-            async profile(profile, tokens) {
-
-                tokens.ext_expires_in = undefined;
-
-                const sotonVerifyData = await axios({
-                    method: 'GET',
-                    url: `https://reliable-sprinkles-18ff83.netlify.app/api/v2/user?sotonId=${profile.email.split('@')[0]}`,
-                    headers: {
-                        Authorization: String(process.env.SOTON_VERIFY_API_AUTH)
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+    return await NextAuth({
+        adapter: PrismaAdapter(prisma),
+        pages: {
+            signIn: '/',
+            signOut: '/',
+        },
+        providers: [
+            AzureADProvider({
+                clientId: String(process.env.AZURE_AD_CLIENT_ID),
+                clientSecret: String(process.env.AZURE_AD_CLIENT_SECRET),
+                tenantId: process.env.AZURE_AD_TENANT_ID,
+                authorization: {
+                    params: {
+                        scope: "openid profile User.Read email",
                     },
-                    data: {
-                        guildId: '1008673689665032233',
+                },
+                async profile(profile, tokens) {
+
+                    tokens.ext_expires_in = undefined;
+
+                    const sotonVerifyData = await axios({
+                        method: 'GET',
+                        url: `https://reliable-sprinkles-18ff83.netlify.app/api/v2/user?sotonId=${profile.email.split('@')[0]}`,
+                        headers: {
+                            Authorization: String(process.env.SOTON_VERIFY_API_AUTH)
+                        },
+                        data: {
+                            guildId: '1008673689665032233',
+                        }
+                    })
+
+                    const data = sotonVerifyData.data;
+
+                    if (data.error) {
+                        res.redirect('https://society.ecs.soton.ac.uk/verify');
                     }
-                })
 
-                const data = sotonVerifyData.data;
-
-                return {
-                    id: data.sotonId,
-                    firstName: data.firstName,
-                    lastName: data.lastName,
-                    displayName: `${data.firstName} ${data.lastName} (${data.sotonId})`,
-                    sotonId: data.sotonId,
-                    discordId: data.discordId,
-                    discordTag: data.discordTag,
+                    return {
+                        id: data.sotonId,
+                        firstName: data.firstName,
+                        lastName: data.lastName,
+                        displayName: `${data.firstName} ${data.lastName} (${data.sotonId})`,
+                        sotonId: data.sotonId,
+                        discordId: data.discordId,
+                        discordTag: data.discordTag,
+                    }
                 }
+            }),
+        ],
+        callbacks: {
+            async session({session, token, user}) {
+                // Send properties to the client, like an access_token from a provider.
+                session.firstName = user.firstName;
+                session.lastName = user.lastName;
+                session.discord = {
+                    tag: user.discordTag,
+                };
+                session.microsoft = {
+                    email: user.sotonId + '@soton.ac.uk',
+                };
+                return session
             }
-        }),
-    ],
-    callbacks: {
-        async session({session, token, user}) {
-            // Send properties to the client, like an access_token from a provider.
-            session.firstName = user.firstName;
-            session.lastName = user.lastName;
-            session.discord = {
-                tag: user.discordTag,
-            };
-            session.microsoft = {
-                email: user.sotonId + '@soton.ac.uk',
-            };
-            return session
         }
-    }
-})
+    })
+}
