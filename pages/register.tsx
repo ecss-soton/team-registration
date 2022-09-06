7978ad3c8df6b4792c0ff2bec0d392cf3d79d43d
miam-miam100 <49870539+miam-miam100@users.noreply.github.com>
2022-09-06 20:56:36 +0100

    Added icons to programming languages.

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,5 +1,97 @@
-import {MultiSelect, NativeSelect, Switch} from '@mantine/core';
-import { IconCheck } from '@tabler/icons';
+import {MultiSelect, NativeSelect, Switch, Box, CloseButton, SelectItemProps, MultiSelectValueProps} from '@mantine/core';
+
+import { forwardRef, FunctionComponent } from 'react';
+import {
+  COriginal,
+  CplusplusOriginal, CsharpOriginal,
+  JavaOriginal,
+  JavascriptOriginal,
+  KotlinPlain, PythonOriginal, RubyPlain,
+  RustPlain, SqlitePlain,
+  TypescriptOriginal
+} from 'devicons-react';
+
+const langData = [
+  { label: 'Rust', value: 'rs' },
+  { label: 'Java', value: 'java' },
+  { label: 'C++', value: 'cpp' },
+  { label: 'Javascript', value: 'js' },
+  { label: 'Typescript', value: 'ts' },
+  {label: 'Kotlin', value: 'kt'},
+  {label: 'C#', value: 'cs'},
+  {label: 'Ruby', value: 'rb'},
+  {label: 'SQL', value: 'sql'},
+  {label: 'Python', value: 'py'},
+  {label: 'C', value: 'c'}
+];
+
+const icons: Record<string, FunctionComponent> = {
+    "rs": RustPlain,
+    "java": JavaOriginal,
+    "cpp": CplusplusOriginal,
+    "js": JavascriptOriginal,
+    "ts": TypescriptOriginal,
+    "kt": KotlinPlain,
+    "cs": CsharpOriginal,
+    "rb": RubyPlain,
+    "sql": SqlitePlain,
+    "py": PythonOriginal,
+    "c": COriginal
+};
+
+function Value({
+  value,
+  label,
+  onRemove,
+  classNames,
+  ...others
+}: MultiSelectValueProps & { value: string }) {
+  const Icon = icons[value];
+  return (
+    <div {...others}>
+      <Box
+        sx={(theme) => ({
+          display: 'flex',
+          cursor: 'default',
+          alignItems: 'center',
+          backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+          border: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[4]}`,
+          paddingLeft: 10,
+          borderRadius: 4,
+        })}
+      >
+        <Box mr={10}>
+          <Icon />
+        </Box>
+        <Box sx={{ lineHeight: 1, fontSize: 12 }}>{label}</Box>
+        <CloseButton
+          onMouseDown={onRemove}
+          variant="transparent"
+          size={22}
+          iconSize={14}
+          tabIndex={-1}
+        />
+      </Box>
+    </div>
+  );
+}
+
+// eslint-disable-next-line react/display-name
+const Item = forwardRef<HTMLDivElement, SelectItemProps>(({ label, value, ...others }, ref) => {
+  // Cannot be undefined
+  const Flag = icons[value as string];
+  return (
+    <div ref={ref} {...others}>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Box mr={10}>
+          <Flag />
+        </Box>
+        <div>{label}</div>
+      </Box>
+    </div>
+  );
+});
+
 
 export default function Home() {
     return (
@@ -14,9 +106,9 @@ export default function Home() {
             />
 
             <MultiSelect
-                data={['Rust', 'Typescript', 'Javascript', 'Java', 'Python', 'C', 'C++', 'C#', 'Ruby', 'Kotlin', 'SQL']}
-                label="What languages do you know"
-                placeholder="Pick all that you like"
+                data={langData}
+                valueComponent={Value}
+                itemComponent={Item}
                 searchable
                 nothingFound="Nothing found"
             />
