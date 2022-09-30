d6242d5e955ee5a3cbee138ea0ccaece135a153f
Euan Caskie <56805259+Ortovoxx@users.noreply.github.com>
2022-09-30 20:43:38 +0100

    Make it work
    (Very hacky thanks @miam-miam100)

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -42,36 +42,41 @@ export const authOptions: NextAuthOptions = {
 };
 
 export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+    // @ts-ignore
     (authOptions.providers[0] as OAuthConfig<AzureADProfile>).profile = async function profile(profile, tokens) {
 
         tokens.ext_expires_in = undefined;
 
-        const sotonVerifyData = await axios({
-            method: 'GET',
-            url: `https://sotonverify.link/api/v2/user?sotonId=${profile.email.split('@')[0]}`,
-            headers: {
-                Authorization: String(process.env.SOTON_VERIFY_API_AUTH)
-            },
-            data: {
-                guildId: '1008673689665032233',
+        let sotonVerifyData;
+
+        try {
+            sotonVerifyData = await axios({
+                method: 'GET',
+                url: `https://sotonverify.link/api/v2/user?sotonId=${profile.email.split('@')[0]}`,
+                headers: {
+                    Authorization: String(process.env.SOTON_VERIFY_API_AUTH)
+                },
+                data: {
+                    guildId: '1008673689665032233',
+                }
+            })
+
+            const data = sotonVerifyData.data;
+
+            return {
+                id: data.sotonId,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                displayName: `${data.firstName} ${data.lastName} (${data.sotonId})`,
+                sotonId: data.sotonId,
+                discordId: data.discordId,
+                discordTag: data.discordTag,
             }
-        })
-
-        const data = sotonVerifyData.data;
-
-        if (data.error) {
+        } catch {
             res.redirect('https://sotonverify.link');
         }
 
-        return {
-            id: data.sotonId,
-            firstName: data.firstName,
-            lastName: data.lastName,
-            displayName: `${data.firstName} ${data.lastName} (${data.sotonId})`,
-            sotonId: data.sotonId,
-            discordId: data.discordId,
-            discordTag: data.discordTag,
-        }
+        return null
     }
     return await NextAuth(req, res, authOptions)
 }
