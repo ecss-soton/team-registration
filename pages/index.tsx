3dea7adfc6ec98fed74f33d1ce97cc37bf9c1128
Euan Caskie <56805259+Ortovoxx@users.noreply.github.com>
2023-02-08 15:26:52 +0000

    Add CV tab

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,15 +9,27 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { NextApiRequestCookies } from "next/dist/server/api-utils";
 import {User,Team} from "@prisma/client";
 import prisma from "../prisma/client";
-import {Button, Text, Image, Tabs, useMantineColorScheme, ActionIcon} from "@mantine/core";
+import {Button, Text, Image, Tabs, useMantineColorScheme, ActionIcon, Badge} from "@mantine/core";
 import Link from "next/link";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faDiscord} from "@fortawesome/free-brands-svg-icons";
-import {IconUsers, IconTimeline, IconSun, IconMoonStars} from "@tabler/icons";
+import {IconUsers, IconTimeline, IconSun, IconMoonStars, IconUserCircle, IconCertificate} from "@tabler/icons";
 import {TeamCard} from "@/components/TeamCard";
+import {CvUpload} from "@/components/CvUpload";
+import {Profile} from "@/components/Profile";
+import {useRouter} from "next/router";
+import {useRef} from "react";
 
 export default function Home({ session, user, url, team }: { session: Session, user: User, url: string, team: Team }) {
+
+    const router = useRouter()
+    let { tab } = router.query
+
+    if (Array.isArray(tab)) tab = tab[0]
+
+    const tabs = ['profile', 'timeline', 'team', 'cv']
+    if (tab && !tabs.includes(tab)) tab = undefined
 
     const { colorScheme, toggleColorScheme } = useMantineColorScheme();
     const dark = colorScheme === 'dark';
@@ -83,10 +95,15 @@ export default function Home({ session, user, url, team }: { session: Session, u
                 </div>
 
 
-                <Tabs defaultValue="timeline" className='w-full'>
+                <Tabs defaultValue={tab || "timeline"} className='w-full'>
                     <Tabs.List position="center">
                         <Tabs.Tab value="timeline" icon={<IconTimeline size={14} />}>Timeline</Tabs.Tab>
                         <Tabs.Tab value="team" icon={<IconUsers size={14} />}>Your Team</Tabs.Tab>
+                        <Tabs.Tab
+                            value="cv"
+                            icon={<IconCertificate size={14} />}
+                            rightSection={<Badge>NEW</Badge>}
+                        >CV Upload</Tabs.Tab>
                     </Tabs.List>
 
                     <Tabs.Panel value="timeline" pt="xs">
@@ -99,6 +116,10 @@ export default function Home({ session, user, url, team }: { session: Session, u
                         {!team && <p>You do not have a team yet</p>}
                         {(team && !team.timeslot) && <p>Book a slot</p>}
                         {(team && team.timeslot) && <p>Your slot is at {team.timeslot}</p>}
+                    </Tabs.Panel>
+
+                    <Tabs.Panel value="cv" pt="xs">
+                        <CvUpload/>
                     </Tabs.Panel>
                 </Tabs>
 
