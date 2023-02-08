d7cdd917d7a5cc2706c421fc185f71d8f43715e1
Euan Caskie <56805259+Ortovoxx@users.noreply.github.com>
2023-02-08 23:52:26 +0000

    Create checkin.ts

---
+++ pages/api/v1/checkin.ts
@@ -0,0 +1,73 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import {unstable_getServerSession} from "next-auth";
+import {authOptions} from "../auth/[...nextauth]";
+import prisma from "../../../prisma/client";
+
+interface ResponseData {
+    success: boolean
+    dietaryReq: string
+    extra: string
+    displayName: string
+}
+
+interface ResponseError {
+    error: boolean
+    message: string
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseData | ResponseError>
+) {
+    if (req.method !== "POST") return res.status(405).json({
+        error: true,
+        message: 'Only HTTP verb POST is permitted',
+    });
+
+    const attemptedAuth = await unstable_getServerSession(req, res, authOptions);
+
+    if (!attemptedAuth?.microsoft.email) {
+        return res.status(400).json({
+            error: true, message: 'You must include the session token with the Authorization header',
+        });
+    }
+
+    if (!String(process.env.ADMINS).split(',').includes(attemptedAuth?.microsoft.email.split('@soton.ac.uk').join(''))) {
+        return res.status(403).json({
+            error: true, message: 'You are not authorized to check people in, naughty naughty!!',
+        });
+    }
+
+
+    const formData: { id: string } = req.body;
+
+    if (!formData.id) {
+        return res.status(400).json({
+            error: true, message: 'Missing id property',
+        });
+    }
+
+    try {
+        const user = await prisma.user.update({
+            data: {
+                checkedIn: true,
+            },
+            where: {
+                id: formData.id
+            }
+        })
+
+        res.status(200).json({
+            success: true,
+            dietaryReq: user.dietaryReq || '',
+            extra: user.extra || '',
+            displayName: user.displayName
+        });
+    } catch (e) {
+        res.status(404).json({
+            error: true,
+            message: "This user does not exist"
+        });
+    }
+
+}
