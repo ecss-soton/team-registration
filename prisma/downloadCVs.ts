d45bef8f02301a6d9b777f92618c7b841667cd9f
Euan Caskie <56805259+Ortovoxx@users.noreply.github.com>
2023-02-08 19:40:37 +0000

    Add script to download all CVs

---
+++ prisma/downloadCVs.ts
@@ -0,0 +1,36 @@
+import prisma from "./client";
+import {writeFileSync,mkdirSync} from 'fs'
+
+(async () => {
+    const users = await prisma.user.findMany({
+        select: {
+            cvFileName: true,
+            firstName: true,
+            lastName: true,
+            sotonId: true,
+        }
+    })
+
+    console.log(`Found ${users.length} users`)
+
+    for (const user of users) {
+        if (!user.cvFileName) continue;
+
+        const data = await prisma.user.findUnique({
+            where: {
+                sotonId: user.sotonId,
+            },
+            select: {
+                cv: true
+            }
+        })
+
+        if (!data?.cv) continue;
+
+        console.log(`Downloaded CV for ${user.sotonId} - ${user.cvFileName}`)
+
+        mkdirSync('./cvs')
+
+        writeFileSync(`./cvs/${user.sotonId}-${user.cvFileName}`, data.cv)
+    }
+})()
